5769617e457969d3a10df4345e409100d133d52e
Naomi-ANN <nevellalex@gmail.com>
2026-03-12 13:58:04 -0700

    Update pc.v

--- pc.v
+++ pc.v
@@ -1,10 +1,9 @@
 //ok so we have clk, and another 32 bit input and 32 bit output according to Fetchtb & image
 // if there is a clk I always need a rst? - idky just do
 
-modeule pc(
-    input  clk,
-    inpust rst, //is this actaully here bc its not on the pic but good practice???
+module pc(
     input [31:0] pc_in,
-    output [31:0] pc_out,
-
-)
+    output [31:0] pc_out
+);
+//pc 
+endmodule
