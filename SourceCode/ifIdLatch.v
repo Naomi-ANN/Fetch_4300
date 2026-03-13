17172daab87ae1e288644ab5da506805a35f1fe0
Marie J. <marijardin10@gmail.com>
2026-03-13 16:36:35 -0700

    finish ifIdLatch.v

--- SourceCode/ifIdLatch.v
+++ SourceCode/ifIdLatch.v
@@ -1,1 +1,18 @@
+module ifIdLatch(
+    input [31:0] addr, data,
+    input clk, rst,
+    output addr_out, data_out
+);
 
+    always @(posedge clk or negedge rst) begin
+        if (rst) begin
+            assign addr_out = 0;
+            assign data_out = 0;
+        end 
+        else begin
+            assign addr_out = addr;
+            assign data_out = data;
+        end
+    end
+
+endmodule
