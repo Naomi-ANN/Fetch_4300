7cc6d89ad749dd233f24b2dd7ce530fbec73dc98
Marie J. <marijardin10@gmail.com>
2026-03-13 16:29:54 -0700

    planning out what fetch.v looks like
    - flow of outputs put into comments

--- SourceCode/Fetch.v
+++ SourceCode/Fetch.v
@@ -1,1 +1,23 @@
+/* Planning of what Instruction Fetch looks like */
 
+/*
+PC:
+- clk and whatever next instruction comes from mux
+
+mux obtains:
+- adder_out
+- external input (i assume this is offset)
+
+adder obtains:
+- pc_out
+- 2'b0000 technically but thats already in built to the module
+
+instr mem obtains:
+- pc_out
+- clk
+
+latch obtains:
+- adder output
+- instr mem output
+- clk
+*/
